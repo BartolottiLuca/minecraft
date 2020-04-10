552be0651169095c0fed8bf0b64c402e18a7080e
luca <luca.bartolotti@gmail.com>
2020-04-10 20:01:13 +0100

    almost done, needs testing

--- start_minecraft.sh
+++ start_minecraft.sh
@@ -6,4 +6,17 @@ set -o pipefail
 # for debug purpose
 # set -o xtrace 
 
-./minecraft.sh start
+os_id=365 # ubuntu 19.04
+region_id=8 # london
+plan_id=401 # 1 CPU 2 GB ram
+shh_key_id=5e8770952ec41 # luca_xps key
+
+vps_id=`curl -H "API-Key: $VULTR_API_KEY" https://api.vultr.com/v1/server/create --data "DCID=$region_id" --data "SSHKEYID=$shh_key_id" --data "VPSPLANID=$plan_id" --data "OSID=$os_id" | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2 `
+sleep 60
+
+IP=`curl -H "API-Key: $VULTR_API_KEY" https://api.vultr.com/v1/server/list#SUBID=$vps_id | awk -F 'main_ip\":' '{print $2}' | cut -d, -f1 | cut -d\" -f2`
+ehco "IP of server $IP"
+echo $IP > /tmp/server_IP
+
+echo "yes" | scp ~/.ssh/id_rsa root@$IP:/root/.ssh/ 
+echo "yes" | ssh root@$IP "sudo apt-get install openjdk-8-jdk-headless -y && git clone git@github.com:BartolottiLuca/minecraft.git && cd minecraft && java -Xms1G -Xmx1G -jar server.jar nogui "
--- stop_minecraft.sh
+++ stop_minecraft.sh
@@ -6,5 +6,9 @@ set -o pipefail
 # for debug purpose
 # set -o xtrace 
 
+IP=`cat /tmp/server_IP` 
+# kill java process
 
-./minecraft.sh stop
+echo "yes" | ssh root@$IP "cd minecraft && git commit -am \"Automathic commit\" && git push"
+
+curl # destroy server
