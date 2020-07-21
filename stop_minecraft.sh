62e8096913fe4b958dbe064d0d09a2b3f41a895d
luca <luca.bartolotti@gmail.com>
2020-07-21 15:01:08 +0100

    update to 1.16.1 and compress the archive, this way it's under 100mb

--- stop_minecraft.sh
+++ stop_minecraft.sh
@@ -13,7 +13,7 @@ vps_id=`cat /tmp/vps_id`
 # kill java process
 ssh -o StrictHostKeyChecking=no root@$IP "~/minecraft/kill_java"
 
-ssh -o StrictHostKeyChecking=no root@$IP "cd ~/minecraft && tar cvf my-server.tar my-server/"
+ssh -o StrictHostKeyChecking=no root@$IP "cd ~/minecraft && tar cvzf my-server.tar my-server/"
 ssh -o StrictHostKeyChecking=no root@$IP "cd ~/minecraft && git commit -am \"Automatic commit\" && git push"
 
 sleep 15
