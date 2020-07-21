8b0b839f297ab695c9db4aa209717cd361cba2b2
luca <luca.bartolotti@gmail.com>
2020-07-21 16:35:22 +0100

    ensure permission on kill_java and ensure push on master

--- stop_minecraft.sh
+++ stop_minecraft.sh
@@ -9,12 +9,13 @@ IP=`cat /tmp/server_IP`
 vps_id=`cat /tmp/vps_id`
 # kill java process
 echo -e "${ORANGE}Try to kill running server${NC}"
-ssh -o StrictHostKeyChecking=no root@$IP "~/minecraft/kill_java"
+ssh -o StrictHostKeyChecking=no root@$IP "chmod +x ~/minecraft/kill_java && ~/minecraft/kill_java"
 
 echo -e "${BLUE}Add, commit and push changes${NC}"
 ssh -o StrictHostKeyChecking=no root@$IP "cd ~/minecraft && git add my-server/"
-ssh -o StrictHostKeyChecking=no root@$IP "cd ~/minecraft && git commit -am \"Automatic commit\" && git push"
+ssh -o StrictHostKeyChecking=no root@$IP "cd ~/minecraft && git commit -m \"Automatic commit\" && git push origin master"
 
 sleep 15
 echo -e "${ORANGE}Destroy Vultr instance${NC}"
 curl -s -H "API-Key: $VULTR_API_KEY" https://api.vultr.com/v1/server/destroy --data "SUBID=$vps_id"
+rm -Rf /tmp/server_IP /tmp/vps_id
